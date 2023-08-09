f33539e7d458d66b5f36f7a01e353498f90af78a
Jordi Tapies <jorditapies@iMac-de-Jordi.local>
2023-08-09 19:27:06 +0200

    lau-web

---
+++ src/Components/Active.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Active = () => {
+  return (
+    <div>
+      <h2>Residencia Activa</h2>
+      <p>
+      La residencia fiscal activa se otorga a las personas que trabajan en Andorra y existen dos
+maneras para obtenerla.
+      </p>
+      <ul>
+        <li>Residencia activa por cuenta propia</li>
+        <p>
+        Autoriza al solicitante a residir y a ejercer una actividad por cuenta propia de forma
+permanente mediante la constitución de una empresa. Se debe fijar la residencia principal
+efectiva en Andorra y residir durante al menos 183 días al año.
+        </p>
+        <li>Residencia activa por cuenta ajena</li>
+        <p>
+        Autoriza al solicitante a residir y a trabajar en el Principado de Andorra de forma permanente
+y efectiva obteniendo un contrato laboral indefinido por una empresa andorrana. Se debe fijar
+la residencia principal efectiva en Andorra y residir durante al menos 183 días al año.
+        </p>
+      </ul>
+    </div>
+  )
+}
+
+export default Active
---
+++ src/Components/Passive.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Passive = () => {
+  return (
+    <div>
+      <h2>Residencia Passiva</h2>
+      <p>Los permisos de residencia pasiva permiten residir fisicamente en Andorra, pero no dan
+derecho a poder trabajar. Existes hasta tres tipos de residencia pasiva que se pueden solicitar
+en Andorra</p>
+<ul>
+  <li>Residencia sin actividad lucrativa</li>
+  <p>
+  Autoriza a residir en el país sin ejercer actividad laboral ni profesional.
+Este tipo de residencia se realiza por inversión en activos andorranos (inversión mínima de
+600.000€) más un depósito reembolsable de 47.500 euros a l’AFA (Agencia Financiera
+Andorrana) y 9.500 euros adicionales por persona a cargo. Se debe establecer la residencia
+principal y efectiva en el Principado de Andorra durante al menos 90 días al año.
+  </p>
+  <li>Residencia para profesionales con proyeccion internacional</li>
+  <p>
+  Este tipo de residencia pasiva en Andorra está dirigida a profesionales con una actividad
+internacional sin apenas intervención en el mercado andorrano. La sede o base de la actividad
+debe estar situada en el Principado de Andorra y al menos el 85 % de los servicios prestados
+deben tener como destinatarios personas o entidades no residentes en el Principado.
+Se deberá entregar un depósito reembolsable en el AFA (Agencia Financiera Andorrana) de
+47.500 euros y 9.500 euros adicionales por casa persona a nombre del titular, y residir en
+Andorra durante al menos 90 días al año
+
+  </p>
+  <li>Residencia por razones de interes cientifico, cultural o deportivo</li>
+  <p>
+  Este tipo de residencia esta dirigida a personas que tienen reputación y acreditación como
+deportista de elite, artista o investigador con prestigio internacional reconocido. Al menos
+un 85 % de los servicios prestados deben ser utilizados en el extranjero. Se deberá
+entregar un depósito reembolsable en el AFA (Agencia Financiera Andorrana) de 47.500
+euros y 9.500 euros adicionales por casa persona a nombre del titular, y residir en Andorra
+durante al menos 90 días al año.
+
+  </p>
+</ul>
+    </div>
+  )
+}
+
+export default Passive
