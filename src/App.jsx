3ccff3dcb8c86345670e0ce450747d7d68a925d3
Laura López Tohà <lauralopeztoha@MacBook-Air-de-Laura.local>
2023-08-17 14:56:08 +0200

    lau-web

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="bg-slate-200 h-screen w-full">
-      <h1 className="text-6xl antialiased font-extrabold">Hilltop Agency Andorra</h1>
+      <h1 className="text-6xl antialiased font-extrabold">Hilltop Agency</h1>
       <h2 className="text-4xl antialiased font-bold">Coming Soon</h2>
       <p className="text-2xl">Podemos ayudarte mientras tanto aqui: </p>
       
