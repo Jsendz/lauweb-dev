adece720e1d565a7c916f5411acc9cbeab3b2e2f
Laura López Tohà <lauralopeztoha@MacBook-Air-de-Laura.local>
2023-08-17 14:55:16 +0200

    lau-web

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-slate-200">
-      <h1 className="text-6xl antialiased">Hilltop Agency Andorra</h1>
-      <h2 className="text-4xl antialiased">Coming Soon</h2>
+    <div className="bg-slate-200 h-screen w-full">
+      <h1 className="text-6xl antialiased font-extrabold">Hilltop Agency Andorra</h1>
+      <h2 className="text-4xl antialiased font-bold">Coming Soon</h2>
       <p className="text-2xl">Podemos ayudarte mientras tanto aqui: </p>
       
 
