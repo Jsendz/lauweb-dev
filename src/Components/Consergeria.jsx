a2452deba15f8e2e0b411dba955b434332d94483
Jordi Tapies <joshuasendzul@gmail.com>
2023-12-06 19:28:04 +0100

    lau-web

--- src/Components/Consergeria.jsx
+++ src/Components/Consergeria.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import {Key3} from '../assets/index.js';
 import {useTranslation} from "react-i18next";
+import Modal from './Modal.jsx';
+
 
 const Consergeria = () => {
 
   const [t] = useTranslation("global");
 
   return (
-    <div name="Consergeria" className="w-full h-full
-     bg-[url('src/assets/background3.jpg')] bg-no-repeat bg-cover py-20">
-       <div className="flex flex-col lg:flex-row lg:justify-around mb-20 max-w-6xl mx-auto rounded-lg bg-white shadow-lg shadow-gray-700">
+    <div name="Consergeria" className="w-full h-full bg-[url('src/assets/background3.jpg')] bg-no-repeat bg-cover py-20">
+      <div className="max-w-7xl 2xl:max-w-[1500px] bg-slate-100 rounded-lg shadow-lg shadow-gray-700 mx-auto">
+       <div className="flex flex-col lg:flex-row lg:justify-around mb-20 mx-auto pt-20 ">
         <div className="w-full ">
-        <h2 className="text-5xl  font-extrabold text-left mb-10 text-slate-800 p-5 pr-0">{t("conser.titulo")}</h2>
-        <p className="text-3xl text-left text-slate-700 p-5">{t("conser.paragrafo")}</p></div>
+        <h2 className="text-5xl  font-extrabold text-left mb-10 text-slate-800 p-5 pr-0 2xl:pl-10">{t("conser.titulo")}</h2>
+        <p className="text-3xl text-left text-slate-700 p-5 2xl:pl-10">{t("conser.paragrafo")}</p></div>
          <div className="w-full">
           <img src={Key3} alt="Set of Keys"  className="mx-auto"/>
          </div>
@@ -21,32 +23,33 @@ const Consergeria = () => {
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.unotitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.unoparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
       </div>
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.dostitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.dosparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
       </div>
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.trestitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.tresparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
       </div>
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.fortitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.forparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
       </div>
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.cinctitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.cincparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
       </div>
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
         <h3 className="text-xl font-extrabold text-left px-5">{t("conser.sistitulo")}</h3>
         <p className="text-left px-5 lg:px-0">{t("conser.sisparagrafo")}</p>
-        <button className="p-5 rounded-lg bg-slate-800 w-full text-white font-bold mt-3">{t("residencia.dosboton")}</button>
+        <Modal />
+      </div>
       </div>
       </div>
         
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -7,7 +7,7 @@ export default function Modal() {
   return (
     <>
       <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-cyan-500 text-slate-800 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mt-1 mb-1 ease-linear transition-all duration-150 w-1/2 mx-auto"
         type="button"
         onClick={() => setShowModal(true)}
       >
@@ -20,7 +20,7 @@ export default function Modal() {
           >
             <div className="relative w-full my-6 mx-auto max-w-xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-xl shadow-lg relative w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                
                 {/*body*/}
@@ -104,7 +104,7 @@ export default function Modal() {
               </div>
             </div>
             <div className="p-2 w-full">
-              <button className="flex mx-auto text-white bg-cyan-500 border-0 py-2 px-8 focus:outline-none hover:bg-cyan-600 rounded text-lg">{t("activa.boton")}
+              <button className="flex mx-auto text-white bg-cyan-500 border-0 py-2 px-8 focus:outline-none hover:bg-cyan-600 rounded text-lg">{t("immo.boton")}
                 
               </button>
             </div>
