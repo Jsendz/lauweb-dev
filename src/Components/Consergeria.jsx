10a4aeaeb804eb8efeb95656ebc574077e3554c4
Jordi Tapies <joshuasendzul@gmail.com>
2023-12-13 17:19:02 +0100

    lau-web

--- src/Components/Consergeria.jsx
+++ src/Components/Consergeria.jsx
@@ -12,12 +12,10 @@ const Consergeria = () => {
     <div name="Consergeria" className="w-full h-full bg-[url('src/assets/background3.jpg')] bg-no-repeat bg-cover py-20">
       <div className="max-w-7xl 2xl:max-w-[1500px] bg-slate-100 rounded-lg shadow-lg shadow-gray-700 mx-auto">
        <div className="flex flex-col lg:flex-row lg:justify-around mb-20 mx-auto pt-20 ">
-        <div className="w-full ">
-        <h2 className="text-5xl  font-extrabold text-left mb-10 text-slate-800 p-5 pr-0 2xl:pl-10">{t("conser.titulo")}</h2>
-        <p className="text-3xl text-left text-slate-700 p-5 2xl:pl-10">{t("conser.paragrafo")}</p></div>
-         <div className="w-full">
-          <img src={Key3} alt="Set of Keys"  className="mx-auto"/>
-         </div>
+        <div className="w-full  ">
+        <h2 className="text-6xl  font-extrabold mb-10 text-slate-800 p-5 pr-0 2xl:pl-10 text-center">{t("conser.titulo")}</h2>
+        <p className="text-4xl  text-slate-700 p-5 2xl:pl-10 text-center">{t("conser.paragrafo")}</p></div>
+         
       </div>
       <div className="w-full flex flex-col lg:flex-row lg:flex-wrap xl:max-w-6xl 2xl:max-w-7xl lg:pl-14 xl:m-auto ">
       <div className="flex flex-col items-left justify-around lg:justify-evenly rounded-lg bg-white p-5 lg:m-2 my-2 shadow-lg shadow-gray-400 lg:w-[30%] w-full xl:h-[50vh] ">
