af2beb815f5acebb8326da86c6d4e9b764ab6cd0
jsendz <joshuasendzul@gmail.com>
2023-12-22 09:27:35 +0100

    lau-web

--- public/assets/index.js
+++ public/assets/index.js
@@ -14,20 +14,20 @@ import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
 import Livy from "./Livy.jpeg";
 import Logo2 from "./logohilltop2.png";
-import Lau from "./Lau3.png";
+import Lau from "./Lau3.webp";
 import Natural from "./natural.png";
 import Res4 from "./residencia.jpg";
 import Empre from "./empresa.jpg";
 import Conser from "./conser.jpg";
 import Immo from "./casa.jpg";
 import Bgblue from "./background3.jpg";
-import consergeria from "./consergeria2.png";
-import residencia from "./docs2.png";
-import immobiliaria from "./immo2.png";
-import empresa from "./empresa2.png";
-import house from "./immocasa3.png";
+import consergeria from "./consergeria2.webp";
+import residencia from "./docs2.webp";
+import immobiliaria from "./immo2.webp";
+import empresa from "./empresa2.webp";
+import house from "./immocasa3.webp";
 import Key3 from "./keys3.png";
-import Sky from "./sky.png";
+import Sky from "./sky.webp";
 
  
 
