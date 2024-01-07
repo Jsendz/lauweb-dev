a3715e8df366711023aa0fd004e61ead761ff159
Laura López Tohà <lauralopeztoha@MacBook-Air-de-Laura.local>
2024-01-07 21:36:17 +0100

    lau-web

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,6 @@ import close from "./close.svg";
 import google from "./google.svg";
 import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
@@ -41,7 +40,6 @@ export {
   google,
   apple,
   arrowUp,
-  discount,
   facebook,
   instagram,
   linkedin,
