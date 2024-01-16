c20ad5173f9a8e63853559fa338411cb51adf633
Jordi Tapies <joshuasendzul@gmail.com>
2024-01-16 19:30:25 +0100

    lau-web

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -70,25 +70,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-        <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-            <ul>
-                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-cyan-600">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
-                        Linkedin<FaLinkedin size={30} />
-                    </a>
-                </li>
-                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-cyan-600">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
-                        Github<FaGithub size={30} />
-                    </a>
-                </li>
-                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-cyan-600">
-                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
-                        Email<HiOutlineMail size={30} />
-                    </a>
-                </li>
-            </ul>
-        </div>
+        
     </nav>
   );
 };
