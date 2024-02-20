9c43589e2e6e4e09d6f1f321adabc69b1abc7798
jsendz <joshuasendzul@gmail.com>
2024-02-20 13:32:15 +0100

    lauweb-dev

--- src/Components/Options2.jsx
+++ src/Components/Options2.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { immobiliaria, consergeria, residencia, empresa } from '../assets/index.js';
 import { Link } from 'react-router-dom';
 import {useTranslation} from "react-i18next";
-import  { useState, useEffect } from 'react';
+import  {  useEffect } from 'react';
 
 
 
 const Options2 = () => {
-    const [isVisible, setIsVisible] = useState(false);
-
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      setIsVisible(scrollY > -1); // You can adjust the scroll threshold
-    };
+    
   
     const scrollToTop = () => {
       window.scrollTo({
@@ -21,12 +16,7 @@ const Options2 = () => {
       });
     };
   
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+    
 
     const [t] = useTranslation("global");
 
@@ -38,44 +28,44 @@ const Options2 = () => {
             <div className='backdrop-blur-lg rounded-xl w-[90%] max-w-3xl mx-auto'>
                 <div className='flex flex-row flex-wrap justify-evenly py-5'>
                 <Link to="/Empresa">
-                {isVisible && (
+               
                 <button onClick={scrollToTop}>
                       <div className=' bg-white rounded-lg flex flex-col justify-around items-center w-[130px] 2xl:w-40 py-3 font-bold mb-3'>
                           <img src={empresa} alt="company"  className='w-[50px] 2xl:w-[75px]'/>
                           <h2 className='text-md lg:text-lg 2xl:text-xl lg:py-2'>{t("inicio.empresa")}</h2>
                       </div>
                       </button>
-                      )}
+                    
                 </Link>
                 <Link to="/Residencia">
-                {isVisible && (
+                
                 <button onClick={scrollToTop}>
                       <div className=' bg-white rounded-lg flex flex-col justify-around items-center w-[130px] 2xl:w-40 py-3 font-bold mb-3'>
                           <img src={residencia} alt="Residence" className='w-[50px] 2xl:w-[75px]' />
                           <h2 className='text-md lg:text-lg 2xl:text-xl lg:py-2'>{t("inicio.residencia")}</h2>
                       </div>
                       </button>
-                      )}
+                      
                 </Link>
                 <Link to="/Inmobiliaria">
-                {isVisible && (
+                
                 <button onClick={scrollToTop}>
                       <div className=' bg-white rounded-lg flex flex-col justify-around items-center w-[130px] 2xl:w-40 py-3 font-bold'>
                           <img src={immobiliaria} alt="Real Estate"  className='w-[50px] 2xl:w-[75px]'/>
                           <h2 className='text-md lg:text-lg 2xl:text-xl lg:py-2'>{t("inicio.immo")}</h2>
                       </div>
                       </button>
-                      )}
+                      
                 </Link>
                 <Link to="/Conserjeria">
-                {isVisible && (
+                
                 <button onClick={scrollToTop}>
                       <div className=' bg-white rounded-lg flex flex-col justify-around items-center w-[130px] 2xl:w-40 py-3 font-bold'>
                           <img src={consergeria} alt="Concierge"  className='w-[50px] 2xl:w-[75px]'/>
                           <h2 className='text-md lg:text-lg 2xl:text-xl lg:py-2'>{t("inicio.conser")}</h2>
                       </div>
                       </button>
-                      )}
+                      
                 </Link>
                 </div>
             </div>
